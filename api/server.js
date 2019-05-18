9486fc777a8f0353df4d3882b276758b28c9e9b1
ykrueng <yasirah.krueng@gmail.com>
2019-05-18 13:31:22 -0500

    add a server

---
+++ api/server.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+
+const server = express()
+
+server.use(express.json())
+server.use(helmet())
+server.use(cors())
+
+server.get('/', (req, res) => res.send('Hello Sever'))
+
+module.exports = server
