8eb19d9297075e1b4e7da13ddba3c2ad45d2e65e
ykrueng <yasirah.krueng@gmail.com>
2019-05-19 09:16:51 -0500

    update migration and seed

--- data/migrations/20190518153952_admin.js
+++ data/migrations/20190518153952_admin.js
@@ -7,6 +7,9 @@ exports.up = function(knex, Promise) {
     tbl.string('lastname', 50).notNullable()
 
     tbl.string('schoolname', 50).notNullable().unique()
+    tbl.string('school_level', 50).notNullable()
+    tbl.string('school_city', 50).notNullable()
+    tbl.string('school_state', 50).notNullable()
 
     tbl
       .string('username', 50)
--- data/seeds/002_admin.js
+++ data/seeds/002_admin.js
@@ -8,42 +8,60 @@ exports.seed = function(knex, Promise) {
         lastname: 'last',
         username: 'admin-1',
         password: 'password123',
-        schoolname: 'schoolname_1'
+        schoolname: 'schoolname_1',
+        school_level: 'K-6',
+        school_city: 'Houston',
+        school_state: 'Texas'
       },
       {
         firstname: 'admin-2',
         lastname: 'last',
         username: 'admin-2',
         password: 'password123',
-        schoolname: 'schoolname_2'
+        schoolname: 'schoolname_2',
+        school_level: 'K-6',
+        school_city: 'Houston',
+        school_state: 'Texas'
       },
       {
         firstname: 'admin-3',
         lastname: 'last',
         username: 'admin-3',
         password: 'password123',
-        schoolname: 'schoolname_3'
+        schoolname: 'schoolname_3',
+        school_level: 'K-6',
+        school_city: 'Houston',
+        school_state: 'Texas'
       },
       {
         firstname: 'admin-4',
         lastname: 'last',
         username: 'admin-4',
         password: 'password123',
-        schoolname: 'schoolname_4'
+        schoolname: 'schoolname_4',
+        school_level: 'K-6',
+        school_city: 'Houston',
+        school_state: 'Texas'
       },
       {
         firstname: 'admin-5',
         lastname: 'last',
         username: 'admin-5',
         password: 'password123',
-        schoolname: 'schoolname_5'
+        schoolname: 'schoolname_5',
+        school_level: 'K-6',
+        school_city: 'Houston',
+        school_state: 'Texas'
       },
       {
         firstname: 'admin-6',
         lastname: 'last',
         username: 'admin-6',
         password: 'password123',
-        schoolname: 'schoolname_6'
+        schoolname: 'schoolname_6',
+        school_level: 'K-6',
+        school_city: 'Houston',
+        school_state: 'Texas'
       }
     ])
   })
