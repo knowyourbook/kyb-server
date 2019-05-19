79544e67fbc29f6b5026fdec9a079016a2f3099e
ykrueng <yasirah.krueng@gmail.com>
2019-05-19 14:45:16 -0500

    fix admin and school db helpers to reflect changes in schema

--- data/helpers/admin.js
+++ data/helpers/admin.js
@@ -3,11 +3,12 @@ const db = require('../config/dbConfig')
 module.exports = {
   get: function (id) {
     let query = db('admin').select(
-      'id',
-      'firstname',
-      'lastname',
-      'username'
-    )
+      'admin.id',
+      'admin.firstname',
+      'admin.lastname',
+      'admin.username',
+      'school.name as school'
+    ).join('school', 'school.id','admin.school_id')
 
     if (id) {
       query.where('id', id).first()
--- data/helpers/school.js
+++ data/helpers/school.js
@@ -1,14 +1,16 @@
 const db = require('../config/dbConfig')
 
 module.exports = {
-  get: function (id) {
-    let query = db('admin').select(
-      'id',
-      'schoolname',
-      'school_level',
-      'school_city',
-      'school_state'
-    )
+  get: function(id) {
+    let query = db('school')
+      .select(
+        'school.id',
+        'school.name',
+        'school.city',
+        'school.state',
+        'school_level.name as level'
+      )
+      .join('school_level', 'school_level.id', 'school.level_id')
 
     if (id) {
       query.where('id', id).first()
