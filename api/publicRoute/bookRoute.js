86450f0afc6210f42c9fbccc9be9d3535552a11c
ykrueng <yasirah.krueng@gmail.com>
2019-05-19 15:11:22 -0500

    add public api/books/:id route

--- api/publicRoute/bookRoute.js
+++ api/publicRoute/bookRoute.js
@@ -3,13 +3,32 @@ const express = require('express')
 const bookDB = require('../../data/helpers/book')
 
 const router = express.Router()
-router.get('/', async (req, res) => {
-  try {
-    const books = await bookDB.get()
-    res.status(200).json(books)
-  } catch (err) {
-    res.sendStatus(500)
-  }
-})
+router
+  .get('/', async (req, res) => {
+    try {
+      const books = await bookDB.get()
+      res.status(200).json(books)
+    } catch (err) {
+      res.sendStatus(500)
+    }
+  })
+  .get('/:id', async (req, res) => {
+    const { id } = req.params
 
+    try {
+      const book = await bookDB.get(Number(id))
+
+      if (book) {
+        res.status(200).json(book)
+      } else {
+        res
+          .status(404)
+          .json({ error: true, message: 'Cannot find book with that ID' })
+      }
+    } catch (err) {
+      res
+        .status(500)
+        .json({ error: true, message: 'Failed to fetch book info' })
+    }
+  })
 module.exports = router
